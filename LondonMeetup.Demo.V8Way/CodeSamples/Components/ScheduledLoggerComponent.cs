289d87846a83f1cfe251aa1054801644bd2eb3a9
Poornima Nayar <poornimakrishnav@gmail.com>
2020-04-21 14:27:40 +0100

    upgraded to 8.6.1

--- LondonMeetup.Demo.V8Way/CodeSamples/Components/ScheduledLoggerComponent.cs
+++ LondonMeetup.Demo.V8Way/CodeSamples/Components/ScheduledLoggerComponent.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using LondonMeetup.Demo.V8Way.CodeSamples.Scheduling;
-using Umbraco.Core;
+using Umbraco.Core.Composing;
 using Umbraco.Core.Logging;
 using Umbraco.Web.Scheduling;
-using IComponent = Umbraco.Core.Composing.IComponent;
 
 namespace LondonMeetup.Demo.V8Way.CodeSamples.Components
 {
     public class ScheduledLoggerComponent : IComponent
     {
-        private ILogger _logger;
+        private readonly ILogger _logger;
+
+        //background task runner is what actually runs a task, you define a background task runner, create a recurring task and add the recurring task to the task runner
         private BackgroundTaskRunner<IBackgroundTask> _loggerRunner;
 
-        public ScheduledLoggerComponent(ILogger logger, IRuntimeState runtime)
+        public ScheduledLoggerComponent(ILogger logger)
         {
             _logger = logger;
             _loggerRunner = new BackgroundTaskRunner<IBackgroundTask>("Scheduled Logger", _logger);
@@ -27,6 +28,14 @@ namespace LondonMeetup.Demo.V8Way.CodeSamples.Components
 
             //As soon as we add our task to the runner it will start to run (after its delay period)
             _loggerRunner.TryAdd(task);
+
+            // you can also call events on background task runners
+            _loggerRunner.TaskCompleted += Task_Completed;
+        }
+
+        private void Task_Completed(BackgroundTaskRunner<IBackgroundTask> sender, TaskEventArgs<IBackgroundTask> e)
+        {
+           _logger.Info<ScheduledLoggerComponent>("Scheduled Logger run finished");
         }
 
         public void Terminate()
