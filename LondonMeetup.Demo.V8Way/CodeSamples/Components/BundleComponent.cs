602d3cd161764b2f9da7801a50422fdb769ed0d9
Poornima Nayar <poornimakrishnav@gmail.com>
2020-06-23 10:23:30 +0100

    bundling and minification example added

---
+++ LondonMeetup.Demo.V8Way/CodeSamples/Components/BundleComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using System.Web.Optimization;
+using Umbraco.Core.Composing;
+
+namespace LondonMeetup.Demo.V8Way.CodeSamples.Components
+{
+    public class BundleComponent : IComponent
+    {
+        public void Initialize()
+        {
+            BundleTable.Bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                "~/css/umbraco-starterkit-style.css"));
+            
+            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/scripts").Include("~/scripts/umbraco-starterkit-app.js"));
+            
+            BundleTable.EnableOptimizations = true;
+        }
+
+        public void Terminate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ LondonMeetup.Demo.V8Way/CodeSamples/Composers/BundleComposer.cs
@@ -0,0 +1,14 @@
+﻿using LondonMeetup.Demo.V8Way.CodeSamples.Components;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace LondonMeetup.Demo.V8Way.CodeSamples.Composers
+{
+    public class BundleComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<BundleComponent>();
+        }
+    }
+}
